5b46c40219d26c29b8495e8741665e3f163174d7
William Gasner <willgasner@gmail.com>
2014-02-17 19:32:17 -0800

    finish static pages

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -34,9 +34,25 @@ describe "signup" do
       before do
         fill_in "Name",         with: "William Gasner"
         fill_in "Email",        with: "willgasner@gmail.com"
-        fill_in "Password",     with: "conute"
-        fill_in "Confirmation", with: "conute"
+        fill_in "Password",     with: "conute1991"
+        fill_in "Confirmation", with: "conute1991"
       end
+
+      # describe "after saving the user" do
+       # before { click_button submit }
+       # let(:user) { User.find_by(email: 'user@example.com') }
+
+        #it { should have_title(user.name) }
+     
+    #   it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+     # end
+
+    #  describe "after submission" do
+     #   before { click_button submit }
+
+      #  it { should have_title('Sign up') }
+       # it { should have_content('error') }
+      #end
 
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
